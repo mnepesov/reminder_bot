5d65d9d9446618b892681759fd5148a73e644379
Mekan Nepesov <nepesowmekan8@gmail.com>
2021-03-29 13:05:10 +0300

    rabbitmq added

---
+++ pkg/rabbitmq/rabbitmq.go
@@ -0,0 +1,19 @@
+package rabbitmq
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"reminder_bot/config"
+)
+
+// Initialize new RabbitMQ connection
+func NewRabbitMQConn(cfg *config.Config) (*amqp.Connection, error) {
+	connAddr := fmt.Sprintf(
+		"amqp://%s:%s@%s:%s/",
+		cfg.RabbitMQ.User,
+		cfg.RabbitMQ.Password,
+		cfg.RabbitMQ.Host,
+		cfg.RabbitMQ.Port,
+	)
+	return amqp.Dial(connAddr)
+}
